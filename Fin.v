e03537f10f51a8bcc724ee79dc3b9cedc64631c7
zhouzhezz <zorichzorich@gmail.com>
2019-09-13 15:13:35 -0400

    add dependent function and product type

--- Fin.v
+++ Fin.v
@@ -18,6 +18,8 @@ Check (Cur 2).
 Check (Inherit 2 (Cur 1)).
 Check (Inherit 2 (Inherit 1 (Cur 0))).
 
+
+(* dependent function *)
 Fixpoint fmax (a : nat) :=
   match a with
   | 0 => Cur 0
@@ -26,3 +28,36 @@ Fixpoint fmax (a : nat) :=
 
 Check (fmax 3).
 Check (fmax 4).
+
+(* Inductive Product (A B:Type) : Type := *)
+(*   Pr : A -> B -> Product A B. *)
+
+(* Check (1, 2). *)
+(* Check (Pr 1 2). *)
+
+
+(* Product Type *)
+Definition indProduct (A B: Type) (C: prod A B -> Type) (p: prod A B) (g : A -> B -> C p): C p :=
+  match p with pair a b => g a b end.
+
+Definition recProduct (A B: Type) (C: Type) (g : A -> B -> C) : prod A B -> C :=
+  fun p => indProduct A B (fun _ => C) p g.
+
+Definition pr₁ (A B : Type) : prod A B -> A :=
+  recProduct A B A (fun a => fun b => a).
+
+Definition pr₂ (A B : Type) : prod A B -> B :=
+  recProduct A B B (fun a => fun b => b).
+
+Definition uniq (A B: Type) (p: prod A B) : Prop :=
+  (pr₁ A B p, pr₂ A B p) = p.
+
+Check (pr₁ nat unit (1, tt)).
+Check (pr₂ nat unit (1, tt)).
+Check (uniq nat unit (1, tt)).
+
+Example uniqExample : (uniq nat unit (1, tt)).
+Proof.
+  reflexivity.
+Qed.
+
