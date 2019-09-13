f8314416f30ee69380d881bbadf9bbdcf9c3dfaf
zhouzhezz <zorichzorich@gmail.com>
2019-09-13 18:01:47 -0400

    Add dependent pair

--- Fin.v
+++ Fin.v
@@ -61,3 +61,37 @@ Proof.
   reflexivity.
 Qed.
 
+(* dependent pair *)
+
+Inductive Sigma (A: Type) (B: A -> Type):=
+| Sig : forall a: A, (B a) -> Sigma A B.
+
+Check Sig.
+
+Inductive FinPlus: nat -> Type :=
+| InheritPlus: forall n, (FinPlus n) -> FinPlus (S n)
+| CurPlus: forall n, FinPlus n.
+
+Fixpoint fPlusMax (a : nat) :=
+  match a with
+  | 0 => CurPlus 0
+  | S a => InheritPlus (a) (fPlusMax a)
+  end.
+
+Check (Sig nat FinPlus 1 (fPlusMax 1)).
+
+Definition indSigma (A: Type) (B: A -> Type) (C: Sigma A B -> Type) (g : forall (a: A) (b: B a), C (Sig A B a b)): forall p : Sigma A B, C p :=
+  fun p => match p with Sig _ _ a b => g a b end.
+
+Definition recSigma (A: Type) (B: A -> Type) (C: Type) (g: forall a: A, B a -> C): forall p: Sigma A B, C :=
+  indSigma A B (fun _ => C) g.
+
+Definition sig₁ (A : Type) (B: A -> Type): Sigma A B -> A :=
+  recSigma A B A (fun a => fun b => a).
+
+Definition sig₂ (A : Type) (B: A -> Type): forall p: Sigma A B, B (sig₁ A B p) :=
+  fun p => indSigma A B (fun p => B (sig₁ A B p)) (fun _ b => b) p.
+
+Definition ac (A B: Type) (R: forall (a: A) (b: B), Type) (g: forall (x: A) , Sigma B (fun y => R x y)) : Sigma (A -> B) (fun f => forall x: A, R x (f x)):=
+  Sig (A -> B) (fun f => forall x: A, R x (f x)) (fun x => sig₁ B (fun y => R x y) (g x)) (fun x => sig₂ B (fun y => R x y) (g x)).
+
